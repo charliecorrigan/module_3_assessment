1bf8a27e15a69e04d379c92327bf731d6f5d5331
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-26 12:07:37 -0600

    Add VCR to best_buy_service_spec

--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 describe "best buy service" do
   it "returns expected data types and keys" do
-    results = BestBuyService.fetch_stores_by_zip(zip)
+    VCR.use_cassette('best_buy_service') do
+      results = BestBuyService.fetch_stores_by_zip(zip)
 
-    expect(results.class).to be_an Array
-    expect(results.first).to be_a Hash
-    expect(results.first).to have_key("storeType")
-    expect(results.first).to have_key("city")
-    expect(results.first).to have_key("distance")
-    expect(results.first).to have_key("phone")
-    expect(results.first).to have_key("longName")
-
+      expect(results.class).to be_an Array
+      expect(results.first).to be_a Hash
+      expect(results.first).to have_key("storeType")
+      expect(results.first).to have_key("city")
+      expect(results.first).to have_key("distance")
+      expect(results.first).to have_key("phone")
+      expect(results.first).to have_key("longName")
+    end
   end
 end
