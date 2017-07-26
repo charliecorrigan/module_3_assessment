64031a640557e69b319b5328b0615d73b1a94426
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-26 11:57:18 -0600

    Create store class method to call total results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   def index
     @stores = Store.fetch_stores_by_zip(params[:search])
-    # response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params[:search]},25))?format=json&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
-    # @stores = JSON.parse(response.body)
+    @total_count = Store.fetch_total_count(params[:search])
   end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -11,7 +11,12 @@ attr_reader :name, :type, :city, :distance, :phone
   def self.fetch_stores_by_zip(zip)
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
     JSON.parse(response.body)["stores"].map do |store_params|
-      Store.new(store_params)
+      store = Store.new(store_params)
     end
+  end
+
+  def self.fetch_total_count(zip)
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
+    JSON.parse(response.body)["total"]
   end
 end
