2acd793e1326c3ccd0524e3486d9d79cfc59626d
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-26 11:24:49 -0600

    Add VCR to test and faraday get to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeType,longName,city,distance,phone&apiKey=7a3heu7emrjz6qbsugmepbv6")
+    binding.pry
+    @stores = response
   end
 end
