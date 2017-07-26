338bc79810e0c045ca92358a5aed0caa2dcee731
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-26 12:04:59 -0600

    continue test setup

--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -2,5 +2,9 @@ require 'rails_helper'
 
 describe "best buy service" do
   it "returns expected data types and keys" do
+    results = BestBuyService.fetch_stores_by_zip(zip)
+
+    expect(results.class).to be_an Array
+    expect(results.first).to be_a Hash
   end
 end
