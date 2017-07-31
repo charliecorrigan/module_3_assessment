7ac90abd3cf6a9f49abe297ac1f9c3c770d52145
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-31 13:52:10 -0600

    Remove comments from store.rb

--- app/models/store.rb
+++ app/models/store.rb
@@ -17,8 +17,6 @@ attr_reader :name, :type, :city, :distance, :phone
 
 
   def self.fetch_total_count(zip)
-    # response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&pageSize=100&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
-    # JSON.parse(response.body)["total"]
     BestBuyService.fetch_total_count(zip)
   end
 end
