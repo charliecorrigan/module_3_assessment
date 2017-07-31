7097ed4531e70fcfdb43e6090a45b639750c5490
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-31 14:14:41 -0600

    Create #fetch_stores_by_zip and #initialize

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,12 +1,16 @@
 class BestBuyService
 
-  def initialize(zip)
+  def initialize
     conn = Faraday.new(:url => 'https://api.bestbuy.com/v1/') 
   end
 
-  def self.fetch_stores_by_zip(zip)
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&pageSize=100&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
+  def fetch_stores_by_zip(zip)
+    response = conn.get("stores(area(#{zip},25))?format=json&pageSize=100&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
     JSON.parse(response.body)["stores"]
+  end
+
+  def self.fetch_stores_by_zip(zip)
+    new.fetch_stores_by_zip(zip)
   end
 
   def self.fetch_total_count(zip)
