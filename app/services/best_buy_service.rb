898df7b69fddb75c779e0f5d32e295a5b086de07
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-31 14:16:16 -0600

    Add instance variable

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,11 +1,11 @@
 class BestBuyService
 
   def initialize
-    conn = Faraday.new(:url => 'https://api.bestbuy.com/v1/') 
+    @conn = Faraday.new(:url => 'https://api.bestbuy.com/v1/') 
   end
 
   def fetch_stores_by_zip(zip)
-    response = conn.get("stores(area(#{zip},25))?format=json&pageSize=100&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
+    response = @conn.get("stores(area(#{zip},25))?format=json&pageSize=100&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
     JSON.parse(response.body)["stores"]
   end
 
