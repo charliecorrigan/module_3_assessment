d34bbd79410a4f6c1563fc6ab3a70af16aee0df5
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-31 14:01:54 -0600

    Add test for #store_by_zip at model level

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 describe Store, type: :model do
-
   it "is valid" do
     store_params = {"storeType": "Mobile SAS",
                   "longName": "Cherry Creek Shopping Center",
@@ -13,4 +12,17 @@ describe Store, type: :model do
     expect(store).to be_a Store
   end
 
+  it "returns expected data from fetch_stores_by_zip" do
+    VCR.use_cassette('model_store_by_zip') do
+      stores = Store.fetch_stores_by_zip(80202)
+
+      expect(stores.count).to eq(17)
+      expect(stores.first).to be_a Store
+      expect(stores.first).to respond_to(:type)
+      expect(stores.first).to respond_to(:name)
+      expect(stores.first).to respond_to(:city)
+      expect(stores.first).to respond_to(:distance)
+      expect(stores.first).to respond_to(:phone)
+    end
+  end
 end
