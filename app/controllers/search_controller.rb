e8514705ec505caa48093941a2ffef60434b63d8
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-31 13:10:29 -0600

    Add pagination gem

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @stores = Store.fetch_stores_by_zip(params[:search])
+    @stores = Store.fetch_stores_by_zip(params[:search]).paginate(:page => params[:page], :per_page => 10)
     @total_count = Store.fetch_total_count(params[:search])
   end
 end
