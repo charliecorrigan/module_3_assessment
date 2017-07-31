37792fedc6dfb7b9a6d5d2f46251ba426f610ac5
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-31 13:23:54 -0600

    Add page size param to api call

--- app/models/store.rb
+++ app/models/store.rb
@@ -9,14 +9,14 @@ attr_reader :name, :type, :city, :distance, :phone
   end
 
   def self.fetch_stores_by_zip(zip)
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&pageSize=100&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
     JSON.parse(response.body)["stores"].map do |store_params|
       store = Store.new(store_params)
     end
   end
 
   def self.fetch_total_count(zip)
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&pageSize=100&show=storeType,longName,city,distance,phone&apiKey=#{ENV['best_buy_api_key']}")
     JSON.parse(response.body)["total"]
   end
 end
