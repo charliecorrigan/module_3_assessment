0fcd83059855a6958cbb81c2b3383334a135ff91
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-31 13:40:47 -0600

    Move fetch_stores_by_zip to service

--- spec/services/api/v1/best_buy_service_spec.rb
+++ spec/services/api/v1/best_buy_service_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe "best buy service" do
   it "returns expected data types and keys" do
     VCR.use_cassette('best_buy_service') do
-      results = BestBuyService.fetch_stores_by_zip(zip)
+      results = BestBuyService.fetch_stores_by_zip(80202)
 
-      expect(results.class).to be_an Array
+      expect(results).to be_an Array
       expect(results.first).to be_a Hash
       expect(results.first).to have_key("storeType")
       expect(results.first).to have_key("city")
